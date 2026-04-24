dc47888511f55e278e55afbd3d6e387d14d2f880
HUDMATT <163348150+HUDMATT@users.noreply.github.com>
2026-04-24 20:04:59 +0000

    update: Completed techniques, and info sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,13 @@ function updateProgressBar() {
 }
 
 window.addEventListener('scroll', updateProgressBar);
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.navbar-nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            scrollToSection(targetId);
+        });
+    });
+});
